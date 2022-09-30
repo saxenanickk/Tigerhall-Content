a4724e809aaa0723cbac0fe1b436765771cd1e93
saxenanickk <saxenanickk@gmail.com>
2022-09-30 14:41:02 +0530

    strings separated out from components

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -3,6 +3,7 @@ import React, {FC} from 'react';
 import {TigerhallText} from '../tigerhallText';
 import {hScale} from '../../utils';
 import {styles} from './styles';
+import {strings} from '../../data/strings/en';
 
 interface Props {
   value: string | undefined;
@@ -12,13 +13,13 @@ interface Props {
 const Search: FC<Props> = ({value, onChangeText}) => (
   <View style={styles.container}>
     <TigerhallText color={'#FFFFFF'} size={hScale(14)} weight={'700'}>
-      Search
+      {strings.search}
     </TigerhallText>
     <TextInput
       style={styles.input}
       placeholderTextColor={'#989898'}
       cursorColor={'#ffffff'}
-      placeholder="Type any keyword"
+      placeholder={strings.type_any_keyword}
       value={value}
       onChangeText={onChangeText}
     />
---
+++ src/data/strings/en.ts
@@ -0,0 +1,6 @@
+const strings = {
+  type_any_keyword: 'Type any keyword',
+  search: 'Search',
+};
+
+export {strings};
