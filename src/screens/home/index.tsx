4c11d518ba21e2b9ef0efb3a9770aa4cc011d871
saxenanickk <saxenanickk@gmail.com>
2022-09-30 14:10:31 +0530

    pagination functionality completed

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,26 +1,94 @@
-import React, {FC} from 'react';
-import {ActivityIndicator, FlatList, View} from 'react-native';
+import React, {FC, useRef, useState} from 'react';
+import {ActivityIndicator, FlatList, ListRenderItem, View} from 'react-native';
 import {styles} from './styles';
-import {ContentCard} from '../../components/contentCard';
+import ContentCard from '../../components/contentCard';
 import {hScale} from '../../utils';
 import {Search} from '../../components/search';
-
-const ListFooterComponent: FC = () => <ActivityIndicator />;
+import {GET_CONTENT_CARDS_QUERY} from '../../data';
+import {useQuery} from '@apollo/client';
 
 const ItemSeparatorComponent: FC = () => <View style={{height: hScale(10)}} />;
 
 const Home: FC = () => {
-  const renderItem = () => <ContentCard />;
+  const [searchKeywords, setSearchKeywords] = useState('');
+  const limit = 5;
+  const offsetRef = useRef(0);
+  const {loading, error, data, fetchMore} = useQuery(GET_CONTENT_CARDS_QUERY, {
+    variables: {
+      offset: 0,
+      limit: limit,
+      keywords: searchKeywords,
+    },
+  });
+
+  const renderItem: ListRenderItem<any> | null | undefined = ({item}) => {
+    const {
+      image: {uri = ''},
+      name = '',
+      categories = '',
+      experts = [],
+    } = item;
+
+    return (
+      <ContentCard
+        image={uri}
+        title={name}
+        category={categories[0]?.name}
+        expert={{
+          firstName: experts[0]?.firstName,
+          lastName: experts[0]?.lastName,
+          company: experts[0]?.company,
+          title: experts[0]?.title,
+        }}
+      />
+    );
+  };
+
+  const onEndReached = () => {
+    offsetRef.current = offsetRef.current + limit;
+    fetchMore({
+      variables: {
+        offset: offsetRef.current,
+      },
+      updateQuery: (prev, {fetchMoreResult}) => {
+        if (!fetchMoreResult) {
+          return prev;
+        }
+        return Object.assign({}, prev, {
+          contentCards: {
+            edges: [
+              ...prev.contentCards.edges,
+              ...fetchMoreResult.contentCards.edges,
+            ],
+          },
+        });
+      },
+    });
+  };
+
+  if (loading) {
+    //
+  }
+
+  if (error) {
+    //
+  }
 
   return (
     <View style={styles.container}>
+      <Search
+        value={searchKeywords}
+        onChangeText={text => setSearchKeywords(text)}
+      />
       <FlatList
-        ListHeaderComponent={Search}
-        ListFooterComponent={ListFooterComponent}
-        contentContainerStyle={styles.listContentContainerStyle}
+        ListFooterComponent={loading ? <ActivityIndicator /> : null}
         ItemSeparatorComponent={ItemSeparatorComponent}
-        data={[1, 2, 3, 4, 5, 6, 7]}
+        contentContainerStyle={styles.listContentContainerStyle}
+        data={data?.contentCards?.edges}
         renderItem={renderItem}
+        onEndReached={onEndReached}
+        onEndReachedThreshold={0.5}
+        keyExtractor={item => item?.id}
       />
     </View>
   );
