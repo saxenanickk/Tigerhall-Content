8ef3333a52db6285680da22604dcf68f0b21778d
saxenanickk <saxenanickk@gmail.com>
2022-09-30 11:34:25 +0530

    TigerhallText component created

---
+++ src/components/tigerhallText/index.tsx
@@ -0,0 +1,45 @@
+import {Text, Platform, TextProps, ColorValue} from 'react-native';
+import React from 'react';
+
+interface Props extends TextProps {
+  children?: React.ReactNode;
+  color?: ColorValue | undefined;
+  weight?:
+    | 'bold'
+    | 'normal'
+    | '100'
+    | '200'
+    | '300'
+    | '400'
+    | '500'
+    | '600'
+    | '700'
+    | '800'
+    | '900'
+    | undefined;
+  size?: number | undefined;
+}
+
+const TigerhallText: React.FC<Props> = ({
+  children,
+  color,
+  weight,
+  size,
+  ...restProps
+}) => {
+  return (
+    <Text
+      style={{
+        /** Font family can be changed to desired font family from here. */
+        fontFamily: Platform.select({ios: 'Avenir', android: 'Robot'}),
+        fontWeight: weight,
+        fontSize: size,
+        color: color,
+      }}
+      {...restProps}>
+      {children}
+    </Text>
+  );
+};
+
+export {TigerhallText};
