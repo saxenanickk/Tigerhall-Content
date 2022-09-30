1e1047d28805beba17c9bc3199a7d90ce4073770
saxenanickk <saxenanickk@gmail.com>
2022-09-30 13:54:27 +0530

    Props added to Search component

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,11 +1,16 @@
 import {View, TextInput} from 'react-native';
-import React from 'react';
+import React, {FC} from 'react';
 import {TigerhallText} from '../tigerhallText';
 import {hScale} from '../../utils';
 import {styles} from './styles';
 
-const Search = () => (
-  <View style={{marginVertical: hScale(14)}}>
+interface Props {
+  value: string | undefined;
+  onChangeText?: ((text: string) => void) | undefined;
+}
+
+const Search: FC<Props> = ({value, onChangeText}) => (
+  <View style={styles.container}>
     <TigerhallText color={'#FFFFFF'} size={hScale(14)} weight={'700'}>
       Search
     </TigerhallText>
@@ -14,6 +19,8 @@ const Search = () => (
       placeholderTextColor={'#989898'}
       cursorColor={'#ffffff'}
       placeholder="Type any keyword"
+      value={value}
+      onChangeText={onChangeText}
     />
   </View>
 );
--- src/components/search/styles.ts
+++ src/components/search/styles.ts
@@ -2,8 +2,10 @@ import {Platform, StyleSheet} from 'react-native';
 import {hScale, wScale} from '../../utils';
 
 const styles = StyleSheet.create({
+  container: {margin: wScale(20), marginVertical: hScale(14)},
   input: {
     fontFamily: Platform.select({ios: 'Avenir', android: 'Robot'}),
+    fontSize: hScale(14),
     backgroundColor: '#003238',
     height: hScale(49),
     color: '#FFFFFF',
